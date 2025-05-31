0f355cfee2191bb87d3b9cafb197151abb4ca689
sayonaraboy2008 <barkamolbaxtiyarovich@gmail.com>
2025-05-31 12:05:47 +0500

    O‘zgartirishlar kiritildi: [telegram uchun havola paydo bo'ldi]

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,19 +4,19 @@ import { FaTelegram } from "react-icons/fa";
 function Header() {
   return (
     <>
-      <header className="flex justify-between items-center md:px-0 px-[20px] max-w-[1280px] m-auto py-[20px]">
+      <header className="flex justify-between items-center xl:px-0 px-[20px] max-w-[1280px] m-auto py-[20px]">
         <Link to={"/"}>
-          <span className="header-logo text-[#282828] font-bold text-[22px]">
+          <span className="header-logo text-[#282828] font-bold text-[16px] min-[310px]:text-[22px]">
             Inamjanov shop
           </span>
         </Link>
-        <ul>
+        <ul className="flex items-center gap-[10px]">
           {/* <li>
             <a href="">Portfolio</a>
           </li> */}
-          <Link to={"https://t.me/Inamjanov_3D"}>
-            <span>Contact vis Telegram</span>
-            <FaTelegram />
+          <Link to={"https://t.me/Inamjanov_3D"} className=" ">
+            {/* <span>Contact vis Telegram</span> */}
+            <FaTelegram className="tg-icon text-[32px]" />
           </Link>
           <Link to={"/contact"} className=" text-[#fff]">
             <li className="bg-black px-[12px] text-[14px] py-[8px] rounded-[10px]">
