bf5c62ab8c0dc5d6d4711f4d7c9f28fc69ce2b18
sayonaraboy2008 <barkamolbaxtiyarovich@gmail.com>
2025-06-05 16:42:23 +0500

    Edit page qo'shildi

--- src/Components/LoadingSpinner.jsx
+++ src/Components/LoadingSpinner.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { RingLoader } from "react-spinners";
+import { HashLoader } from "react-spinners";
 
 const LoadingSpinner = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/70 dark:bg-black/50 backdrop-blur-sm">
-      <RingLoader color="#3B82F6" size={80} speedMultiplier={1} />
+      <HashLoader color="#3B82F6" size={80} speedMultiplier={1} />
     </div>
   );
 };
